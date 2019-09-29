4a7d12a6923a2934fee91e8b10f99cde64c52e7d
league <league@imac-14.attlocal.net>
2019-09-29 15:56:53 -0700

    finished exception method

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,24 +1,28 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	public double divide(double a, double b) throws IllegalArgumentException{
-		
+	public double divide(double a, double b) throws IllegalArgumentException {
+
 		try {
-			return a/b;
-		}catch(IllegalArgumentException e){
+			return a / b;
+		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException("Cannot divide by 0");
 		}
-		
+
 	}
-	
-	public String reverseString(String str) throws IllegalStateException{
+
+	public String reverseString(String str) throws IllegalStateException {
 		try {
-			return str;
-		}catch(IllegalStateException e){
+			String news = "";
+			for (int i = 0; i < str.length(); i++) {
+				news = news + str.substring(str.length() - i);
+			}
+			return news;
+		} catch (IllegalStateException e) {
 			e.printStackTrace();
 			throw new IllegalStateException("String length is 0");
 		}
 	}
-	
+
 }
